86a9cbf116c8cabbeac3ce386ca731777386b59a
Hyuntae Lee <lht@huvitz.com>
2019-08-21 10:10:57 +0900

    first commit

---
+++ MFCApplication1/libAngioViewer/Viewer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace libAngioViewer
+{
+    public class Viewer
+    {
+        public static void open_viewer()
+        {
+            MessageBox.Show("open_viewer() has been called.");
+        }
+    }
+}
---
+++ MFCApplication1/libAngioViewerCLR/libAngioViewerCLR.h
@@ -0,0 +1,22 @@
+// libAngioViewerCLR.h
+
+#pragma once
+
+using namespace System;
+using namespace System::Reflection;
+
+namespace libAngioViewerCLR {
+
+	public ref class Viewer
+	{
+	public:
+		static void open_viewer() {
+			libAngioViewer::Viewer::open_viewer();
+		}
+	};
+}
+
+__declspec(dllexport) void open_angio_viewer()
+{
+	libAngioViewerCLR::Viewer::open_viewer();
+}
